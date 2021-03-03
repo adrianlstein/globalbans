9d6bc9414b9d19e8d5604ed814179a05c9c56348
adrianlukasstein@gmail.com <adrianlukasstein@gmail.com>
2021-03-03 22:50:36 +0100

    display amount of users on the watch and blacklist

--- commands/dev/stats.js
+++ commands/dev/stats.js
@@ -16,45 +16,58 @@ class Stats extends Command {
 
     async run(client, message) {
         let cpuCores = cpus().length
-        let bannedUserCount = null
+        
+        client.bdd.query('SELECT COUNT(*) AS namesCount FROM user_blacklist', async function (err, result) {
+            if (err) throw err
+            
+            let bannedUserCount = result[0].namesCount
 
-        let queryResult = await client.bdd.query('SELECT COUNT(*) AS namesCount FROM user_blacklist')
+            client.bdd.query('SELECT COUNT(*) AS namesCount FROM user_watchlist', async function (err, result) {
+                if (err) throw err
 
-        bannedUserCount = queryResult[0].namesCount
-        await message.channel.send({
-            embed: {
-                title: client.user.username,
-                timestamp: new Date(),
-                footer: {
-                    icon_url: client.user.displayAvatarURL(),
-                    text: client.user.username
-                },
-                thumbnail: {
-                    url: client.user.displayAvatarURL()
-                },
-                fields: [
-                    {
-                        name: 'My users ',
-                        value: 'Users:' + `\`\`${client.users.cache.size}\`\``,
-                        inline: true
-                    },
-                    {
-                        name: 'Processor use',
-                        value: `${(loadavg()[0]/cpuCores).toFixed(2)}% / 100%`,
-                        inline: true
-                    },
-                    {
-                        name: 'RAM use',
-                        value: `${Math.trunc((process.memoryUsage().heapUsed) / 1000 / 1000)} MB / ${Math.trunc(totalmem() / 1000 / 1000)} MB`,
-                        inline: true
-                    },
-                    {
-                        name: 'Users Blacklisted',
-                        value: bannedUserCount,
-                        inline: true
+                let userWatchlistCount = result[0].namesCount
+
+                await message.channel.send({
+                    embed: {
+                        title: client.user.username,
+                        timestamp: new Date(),
+                        footer: {
+                            icon_url: client.user.displayAvatarURL(),
+                            text: client.user.username
+                        },
+                        thumbnail: {
+                            url: client.user.displayAvatarURL()
+                        },
+                        fields: [
+                            {
+                                name: 'My users ',
+                                value: 'Users:' + `\`\`${client.users.cache.size}\`\``,
+                                inline: true
+                            },
+                            {
+                                name: 'Processor use',
+                                value: `${(loadavg()[0]/cpuCores).toFixed(2)}% / 100%`,
+                                inline: true
+                            },
+                            {
+                                name: 'RAM use',
+                                value: `${Math.trunc((process.memoryUsage().heapUsed) / 1000 / 1000)} MB / ${Math.trunc(totalmem() / 1000 / 1000)} MB`,
+                                inline: true
+                            },
+                            {
+                                name: 'Users Blacklisted',
+                                value: bannedUserCount,
+                                inline: true
+                            },
+                            {
+                                name: 'Users Watchlist',
+                                value: userWatchlistCount,
+                                inline: true
+                            }
+                        ]
                     }
-                ]
-            }
+                })
+            })
         })
     }
 }
